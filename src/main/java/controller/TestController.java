b7bfe983791f7778fc083e8a2a7e5366edc05a0a
Трапезников Лев Александрович <LAlTrapeznikov@sberbank.ru>
2022-05-09 00:36:59 +0300

    HelloSpring

---
+++ src/main/java/controller/TestController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import model.HelloResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String helloWorld(String name){
+        return "Минай чтоб ты сдох сука!!!".toUpperCase(Locale.ROOT);
+    }
+
+/*
+example post : curl -X 'POST' -d {"lev"} \http://localhost:8080/post
+- d(содержимое запроса) {"String name"}
+ */
+    @PostMapping("/post")
+    public HelloResponse hello(@RequestBody String name){
+        HelloResponse hl = new HelloResponse();
+        hl.setMessage("Hello " + name);
+        return hl;
+    }
+
+    /*
+     curl -X 'GET' \http://localhost:8080/lev?v=2;
+     v- it's version = 0,1,2 + return getMessage;
+
+     */
+     @RequestMapping(value = "/{name}",method = RequestMethod.GET)
+    public HelloResponse Version(@PathVariable String name, @RequestParam(value = "v",required = false)Integer version){
+        HelloResponse response = new HelloResponse();
+        switch (version){
+            case 0:  response.setMessage("version 0");
+            break;
+            case 1: response.setMessage(name + " Hello version : 1");
+            break;
+            case 2: response.setMessage(" Hi, nice to meet you " + name + ", you version application 2");
+            break;
+        }
+        return response;
+     }
+}
