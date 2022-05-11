84c471cf91367ba979aad5fe63a3f847677c51c0
Трапезников Лев Александрович <LAlTrapeznikov@sberbank.ru>
2022-05-11 13:23:50 +0300

    refactoring version method

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -25,26 +25,26 @@ public class TestController {
     }
 
     /*
-     curl -X 'GET' \http://localhost:8080/post?v=2;
+     curl -X 'GET' \http://localhost:8080/get?v=2;
      v- it's version = 0,1,2 + return getMessage;
 
      */
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    public HelloResponse Version(@PathVariable String name, @RequestParam(value = "v", required = false) Integer version) {
+    @RequestMapping(value = "/{get}", method = RequestMethod.GET)
+    public HelloResponse Version(@PathVariable String get, @RequestParam(value = "v", required = false) String version) {
         HelloResponse response = new HelloResponse();
-        if (version == null) {
+        if (version.isEmpty() || Integer.parseInt(version) <= 0) {
             response.setMessage("Please make sure your request is correct");
             return response;
         }else {
             switch (version) {
-                case 0:
-                    response.setMessage("version 0");
+                case "1":
+                    response.setMessage("version : " + version);
                     break;
-                case 1:
-                    response.setMessage(name + " Hello version : 1");
+                case "2":
+                    response.setMessage("Hello version : " + version);
                     break;
-                case 2:
-                    response.setMessage(" Hi, nice to meet you " + name + ", you version application 2");
+                case "3":
+                    response.setMessage(" Hi, nice to meet you, you version application: " + version);
                     break;
                 default:
                     response.setMessage("Oops, sorry, your version is not unknown");
